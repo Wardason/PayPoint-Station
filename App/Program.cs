6d3740122750b0d7b61022fbbb05a4c7ee5ba6c6
Leonard <66140015+Wardason@users.noreply.github.com>
2023-08-18 15:28:13 +0200

    structuring app and ui

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PayPoint_Station.UI;
+
+namespace PayPoint_Station.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppScreen.Welcome();
+        }
+    }
+}
---
+++ UI/AppScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayPoint_Station.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            Console.WriteLine("\n\n------------------------------PayPoint------------------------\n\n");
+            Console.WriteLine("Insert your ATM Card");
+            Console.WriteLine("Note: Actual ATM system will accept and validate physical ATM card");
+            Console.WriteLine("Mote: For Creating an Account, press 'a' to continue\n");
+            Console.WriteLine("\n\nPress enter to continue.\n");
+            Console.ReadLine();
+        }
+    }
+}
